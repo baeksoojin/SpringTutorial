f09e59db0e42974914389683ef70bbac9e03fcf1
sujin-create <qsoojinq@naver.com>
2023-01-05 17:06:11 +0900

    Refactor : discount policy 변화에 따른 service code 변경

--- core/src/main/java/tutorial/core/order/OrderServiceImpl.java
+++ core/src/main/java/tutorial/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package tutorial.core.order;
 
 import tutorial.core.discount.DiscountPolicy;
 import tutorial.core.discount.FixDiscountPolicy;
+import tutorial.core.discount.RateDiscountPolicy;
 import tutorial.core.member.Member;
 import tutorial.core.member.MemberRepository;
 import tutorial.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import tutorial.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements  OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy; //interface에만 의존하고 구체화에는 의존하지 않도록 DIP 원칙을 가져감
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
