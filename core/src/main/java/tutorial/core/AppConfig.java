8f08210380ce73b1eee9ba5d9efcd927394abc74
sujin-create <qsoojinq@naver.com>
2023-01-05 17:51:14 +0900

    Refactor : AppConfig에서 역할과 구현이 한 눈에 들어오도록 작성

--- core/src/main/java/tutorial/core/AppConfig.java
+++ core/src/main/java/tutorial/core/AppConfig.java
@@ -1,6 +1,8 @@
 package tutorial.core;
 
+import tutorial.core.discount.DiscountPolicy;
 import tutorial.core.discount.FixDiscountPolicy;
+import tutorial.core.member.MemberRepository;
 import tutorial.core.member.MemberService;
 import tutorial.core.member.MemberServiceImpl;
 import tutorial.core.member.MemoryMemberRepository;
@@ -10,11 +12,20 @@ import tutorial.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }//생성자 주입
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }//생성자주입
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
 
 }
