3f54b9040b8364ca484a00f9d1816baa40b5c6a6
sujin-create <qsoojinq@naver.com>
2023-01-18 00:05:33 +0900

    Refactor : spring에 의존하지 않는 java제공 annotation방법으로 생명주기 콜백 설정

--- core/src/test/java/tutorial/core/lifecycle/NetworkClient.java
+++ core/src/test/java/tutorial/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package tutorial.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
 
     private String url;
@@ -29,11 +32,13 @@ public class NetworkClient{
         System.out.println("close: "+url);
     }
 
+    @PostConstruct
     public void init() throws Exception {
         connect();
         call("초기화 연결 메시지");
     }//properties들이 세팅이 끝나면 호출(의존관계 주입이 끝나면 호출)
 
+    @PreDestroy
     public void close() throws Exception {
         disconnect();
     }
