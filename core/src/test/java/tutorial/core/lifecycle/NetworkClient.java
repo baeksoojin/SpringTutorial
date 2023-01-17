4d8c956af555d4ea6a954ea553fd52026fd7f66a
sujin-create <qsoojinq@naver.com>
2023-01-17 23:50:49 +0900

    Fix : 초기화, 소멸 method 적용

--- core/src/test/java/tutorial/core/lifecycle/NetworkClient.java
+++ core/src/test/java/tutorial/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package tutorial.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient(){
-        System.out.println("생성자 호출, ulr = "+ url);
-        connect();
-        call("초기화 연결 메시지");
+        System.out.println("생성자 호출, url = " + url);
     }
 
     public void setUrl(String url){
@@ -20,11 +21,22 @@ public class NetworkClient {
     }
 
     public void call(String message){
-        System.out.println("call : "+ url +"message = "+ message);
+        System.out.println("call : "+ url +" message = "+ message);
     }
 
     //서비스 종료시 호출
     public void disconnect(){
-        System.out.println("clase: "+url);
+        System.out.println("close: "+url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }//properties들이 세팅이 끝나면 호출(의존관계 주입이 끝나면 호출)
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
