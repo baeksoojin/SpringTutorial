ab2771442432ee081549dbe2b8738f0137569898
sujin-create <qsoojinq@naver.com>
2023-01-18 17:33:48 +0900

    Test : singleton과의 비교를 통한 prototype scope

---
+++ core/src/test/java/tutorial/core/scope/PrototypeTest.java
@@ -0,0 +1,45 @@
+package tutorial.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 "+prototypeBean1);
+        System.out.println("prototypeBean2 "+prototypeBean2);
+
+        //destroy가 호출되지 않는다.
+        // 호출할 필요가 있다면 .destroy로 직접 종료 메서드사용
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.close");
+        }
+    }
+
+
+}
