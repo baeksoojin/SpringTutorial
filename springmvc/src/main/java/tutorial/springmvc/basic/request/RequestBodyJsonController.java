3b82abea80f91a7c814379d757c42c0e69349a9e
sujin-create <qsoojinq@naver.com>
2023-02-09 11:22:34 +0900

    body를 통한 request data처리

---
+++ springmvc/src/main/java/tutorial/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,20 @@
+package tutorial.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tutorial.springmvc.basic.HelloData;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return "ok";
+    }
+}
