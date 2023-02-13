23b9bf17a4c9339b2528320d04d7687add3f3710
sujin-create <qsoojinq@naver.com>
2023-02-14 02:39:00 +0900

    Add : itemcontroller/동적html

---
+++ itemservice/src/main/java/tutorial/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,105 @@
+package tutorial.itemservice.web.basic;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import tutorial.itemservice.domain.Item;
+import tutorial.itemservice.domain.ItemRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item,Model model){
+
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute() Item item,Model model){
+
+        itemRepository.save(item);
+        return "basic/item";
+    }//이름을 생략한 버전. name속성은 Item class의 첫글자를 소문자로 변경
+
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "basic/item";
+    }//@ModelAttribute자체를 생략
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+
+
+
+
+
+
+    //test용 데이터 추가
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000,20));
+    }
+
+}
