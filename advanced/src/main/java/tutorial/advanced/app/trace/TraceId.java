a1689cebfe550454b512b66bb01a94b1e84dbdfb
sujin-create <qsoojinq@naver.com>
2023-03-06 16:20:15 +0900

    log prototype

---
+++ advanced/src/main/java/tutorial/advanced/app/trace/TraceId.java
@@ -0,0 +1,42 @@
+package tutorial.advanced.app.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+    private String id;
+    private int level;
+
+    public TraceId(){
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level){
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId(){
+        return UUID.randomUUID().toString().substring(0,8);//앞의 8자리만 사용하기 위해서 substring
+    }
+
+    private TraceId createNextId(){
+        return new TraceId(id, level+1);
+    }
+
+    private TraceId createPreviousId(){
+        return new TraceId(id, level-1);
+    }
+
+    public boolean isFirstLevel(){
+        return level==0;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+}
---
+++ advanced/src/main/java/tutorial/advanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,19 @@
+package tutorial.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderService orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId){
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
