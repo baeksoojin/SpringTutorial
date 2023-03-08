2e2461d89e18d1f327e3ad607c48780f44617f2c
sujin-create <qsoojinq@naver.com>
2023-03-08 13:06:26 +0900

    Test:  익명내부 클래스 활용 template pattern 적용

--- advanced/src/test/java/tutorial/advanced/app/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/tutorial/advanced/app/trace/template/TemplateMethodTest.java
@@ -42,5 +42,27 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.execute();
+
+
+    }
+
 
 }
