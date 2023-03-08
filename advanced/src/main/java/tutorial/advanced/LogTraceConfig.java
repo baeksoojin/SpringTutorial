c893f1d39e7f0ad8c62f579fd8cf8f29dec46280
sujin-create <qsoojinq@naver.com>
2023-03-08 12:37:29 +0900

    ADD : change logtrace

--- advanced/src/main/java/tutorial/advanced/LogTraceConfig.java
+++ advanced/src/main/java/tutorial/advanced/LogTraceConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tutorial.advanced.app.trace.logtrace.FieldLogTrace;
 import tutorial.advanced.app.trace.logtrace.LogTrace;
+import tutorial.advanced.app.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
